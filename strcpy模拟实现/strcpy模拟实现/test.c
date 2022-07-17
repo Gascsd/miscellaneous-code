7f3273cf1b789e31c2cc0f447681ed53f015f4ec
gascsd <3311922913@qq.com>
2022-07-17 22:34:40 +0800

    strcpy模拟实现

--- strcpy模拟实现/strcpy模拟实现/test.c
+++ strcpy模拟实现/strcpy模拟实现/test.c
@@ -1,1 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+char* my_strcpy(char* strDestination, const char* strSource)
+{
+	char* ptr = strDestination;
+	while (*strDestination++ = *strSource++);
+	return ptr;
+}
+
+int main()
+{
+	char arr1[] = "abcdef";
+	char arr2[10] = { 0 };
+	printf("%s\n", my_strcpy(arr2, arr1));
+	return 0;
+}
