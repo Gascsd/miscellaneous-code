d41be4888f25610b0c0eec960cd0256ccd3caa0a
gascsd <3311922913@qq.com>
2022-07-17 21:06:20 +0800

    strlen模拟实现（多种方法）

--- strlen模拟实现/strlen模拟实现/test.c
+++ strlen模拟实现/strlen模拟实现/test.c
@@ -1,22 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<assert.h>
 
-size_t my_strlen(const char* string)
+size_t my_strlen1(const char* string)//计数器法
 {
-	assert(string);
-	int count = 0;
-	while (*string != '\0')
+	size_t count = 0;
+	while (*string++ != '\0')
 	{
 		count++;
-		string++;
 	}
 	return count;
+}
+
+size_t my_strlen2(const char* string)//递归法
+{
+	if (*string == '\0')
+		return 0;
+	else
+		return 1 + my_strlen2(string + 1);
+}
+
+size_t my_strlen3(const char* string)//指针-指针法
+{
+	const char* ptr = string;
+	while (*ptr != '\0')
+		ptr++;
+	return (size_t)(ptr - string);
 }
 int main()
 {
 	char arr1[] = "abcdef";
-	size_t ret = my_strlen(arr1);
-	printf("%zu", ret);
+	size_t ret = my_strlen1(arr1);
+	printf("%zu\n", ret);
+	ret = my_strlen2(arr1);
+	printf("%zu\n", ret);
+	ret = my_strlen3(arr1);
+	printf("%zu\n", ret);
+
 	return 0;
 }
