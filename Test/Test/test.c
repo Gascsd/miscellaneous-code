cbc5113cf3c242bdf88d0eb14a932e47aaff2be2
@gascsd <3311922913@qq.com>
2022-11-21 13:54:49 +0800

    近期学习记录

--- Test/Test/test.c
+++ Test/Test/test.c
@@ -84,53 +84,131 @@
 //	return 0;
 //}
 
+//#include<stdio.h>
+//#include<ctype.h>
+//int main()
+//{
+//	char arr[] = "AAABBBCCCaaaabbbcc";
+//	int count[52] = { 0 };
+//	int i = 0;
+//	for (int i = 0; i < sizeof(arr) - 1; i++)//遍历数组arr
+//	{
+//		if (isupper(arr[i]))//大写字母的情况
+//		{
+//			//假设大写字母在数组的前半部分
+//			count[arr[i] - 'A']++;
+//		}
+//		else if (islower(arr[i]))
+//		{
+//			//小写字母在后半部分
+//			count[arr[i] - 'a' + 26]++;
+//		}
+//		else
+//		{
+//			//防止输入错误
+//			printf("输入的内容中有非字母存在，退出程序\n");
+//			return -1;
+//		}
+//	}
+//	//找到数组count中最大值的下标
+//	int max = count[0];
+//	int index = 0;
+//	for (int i = 0; i < 52; i++)
+//	{
+//		if (max < count[i])
+//		{
+//			max = count[i];
+//			index = i;
+//		}
+//	}
+//	//下标还原为字符
+//	char ret;
+//	if (index < 26)//大写字母
+//	{
+//		ret = index + 'A';
+//	}
+//	else
+//	{
+//		ret = index + 'a' - 26;
+//	}
+//	printf("出现次数最多的字母为：%c", ret);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int factorial(int n)//求阶乘
+//{
+//	int ret = 1;
+//	for (int i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//	}
+//	return ret;
+//}
+//int Cmn(int m, int n)
+//{
+//	int Ann = factorial(n);
+//	int Amm = factorial(m);
+//	int An_m = factorial(n - m);
+//	return Ann / (Amm * An_m);
+//}
+//int main()
+//{
+//	int m, n;
+//	printf("input m,n:");
+//	scanf("%d %d", &m, &n);
+//	int ret = Cmn(m, n);
+//	printf("%d!/(%d!*(%d-%d)!)=%d", n, m, n, m, ret);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//
+//float ff(float* x, float* y, float* z)
+//{
+//	return *x + *y + *z;
+//}
+//
+//int main()
+//{
+//	int a[5] = { 1,2,3,4,5 }, * p, i;
+//	for (p = a; p < a + 5; p++)
+//	{
+//		printf("%3d", ++(*p));
+//	}
+//	printf("\n");
+//	for (p = a; p < a + 5; p++)
+//	{
+//		printf("%3d", ++ * p);
+//	}
+//	printf("\n");
+//
+//	return 0;
+// }
+
 #include<stdio.h>
-#include<ctype.h>
+void mov(int* x, int n, int m)
+{
+	int i, j;
+	for (i = 0; i < m; i++)
+	for (j = n - 1;; j > 0; j--)
+		x[j] = x[j - 1];
+	x[0] = x[n - 1];
+}
+
 int main()
 {
-	char arr[] = "AAABBBCCCaaaabbbcc";
-	int count[52] = { 0 };
-	int i = 0;
-	for (int i = 0; i < sizeof(arr) - 1; i++)//遍历数组arr
+	int m, n, i, a[80], * p;
+	printf("Input n,m:");
+	scanf("%d%d", &m, &n);
+	for (p = a, i = 0; i < 10; i++)
+		scanf("%d", &p++);
+	mov(a, n, m);
+	printf("After move : ");
+	for (i = 0; i < 10; i++)
 	{
-		if (isupper(arr[i]))//大写字母的情况
-		{
-			//假设大写字母在数组的前半部分
-			count[arr[i] - 'A']++;
-		}
-		else if (islower(arr[i]))
-		{
-			//小写字母在后半部分
-			count[arr[i] - 'a' + 26]++;
-		}
-		else
-		{
-			//防止输入错误
-			printf("输入的内容中有非字母存在，退出程序\n");
-			return -1;
-		}
+		printf("%5d", a[i]);
 	}
-	//找到数组count中最大值的下标
-	int max = count[0];
-	int index = 0;
-	for (int i = 0; i < 52; i++)
-	{
-		if (max < count[i])
-		{
-			max = count[i];
-			index = i;
-		}
-	}
-	//下标还原为字符
-	char ret;
-	if (index < 26)//大写字母
-	{
-		ret = index + 'A';
-	}
-	else
-	{
-		ret = index + 'a' - 26;
-	}
-	printf("出现次数最多的字母为：%c", ret);
 	return 0;
 }
