d514d551c03ca17c7fa7c85c7e24571cde1a0acf
Gascsd <3311922913@qq.com>
2023-03-15 12:48:06 +0800

    一些测试代码

--- test_3_13/test_3_13/test.cpp
+++ test_3_13/test_3_13/test.cpp
@@ -3,35 +3,46 @@
 #define N 10
 
 #include<iostream>
+#include<string>
 using namespace std;
 
-template<class T>
-class Array
-{
-public:
-	T& operator[](size_t i)
-	{
-		//assert(i < N);
-		return _a[i];
-	}
-private:
-	T _a[N];
-};
+//template<class T>
+//class Array
+//{
+//public:
+//	T& operator[](size_t i)
+//	{
+//		//assert(i < N);
+//		return _a[i];
+//	}
+//private:
+//	T _a[N];
+//};
+//int main()
+//{
+//	Array<int> a;
+//	for (size_t i = 0 ; i < N; ++i)
+//	{
+//		a[i] = i;
+//	}
+//	for (size_t i = 0; i < N; ++i)
+//	{
+//		cout << a[i] << " ";
+//	}
+//	cout << endl;
+//	return 0;
+//}
+
+
 int main()
 {
-	Array<int> a;
-	for (size_t i = 0 ; i < N; ++i)
-	{
-		a[i] = i;
-	}
-	for (size_t i = 0; i < N; ++i)
-	{
-		cout << a[i] << " ";
-	}
-	cout << endl;
+	string s1("hello world");
+	s1.resize(5);
+	cout << s1.size() << endl;
+	cout << s1.capacity() << endl;
+	cout << s1 << endl;
 	return 0;
 }
-
 
 
 
