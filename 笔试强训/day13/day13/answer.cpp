e069a4391da3ba640b12b8e099668d55a9b3b787
Gascsd <3311922913@qq.com>
2023-04-04 10:42:10 +0800

    笔试强训day13跳石板解法

--- 笔试强训/day13/day13/answer.cpp
+++ 笔试强训/day13/day13/answer.cpp
@@ -54,33 +54,53 @@
 //}
 
 //跳石板
-
 #include <iostream>
-
+#include <vector>
+#include <math.h>
 using namespace std;
-
-int GetMaxFactor(int n)
+void get_div(int num, vector<int>& div)
 {
-    for (int i = 2; i < n; ++i)
+    for (int i = 2; i <= sqrt(num); i++)
     {
-        if (n % i == 0)
-            return n / i;
+        if (num % i == 0)
+        {
+            div.push_back(i);
+            if (i != num / i)
+                div.push_back(num / i);
+        }
     }
-    return -1;
+}
+int Jump(int n, int m)
+{
+    vector<int> step(m + 1, -1);//-1表示不能够跳到
+    step[n] = 0;//初始化当前位置
+    for (int i = n; i <= m; i++)
+    {
+        if (step[i] == -1)
+            continue;
+        vector<int> div;
+        get_div(i, div);
+        for (size_t j = 0; j < div.size(); ++j)
+        {
+            //同一个位置被多种走法走到，找最小的方法
+            if (div[j] + i <= m && step[div[j] + i] != -1)
+            {
+                step[div[j] + i] = step[div[j] + i] < step[i] + 1 ? step[div[j] + i] : step[i] + 1;
+            }
+            else if (div[j] + i <= m)
+            {
+                step[div[j] + i] = step[i] + 1;
+            }
+        }
+    }
+    return step[m];
 }
 int main()
 {
     int n, m;
     cin >> n >> m;
-    int a = 0;
-    int count = 0;
-    while (n < m)
-    {
-        a = GetMaxFactor(n);
-        n += a;
-        ++count;
-    }
-
+    int min_step = Jump(n, m);
+    cout << min_step << endl;
     return 0;
 }
 
