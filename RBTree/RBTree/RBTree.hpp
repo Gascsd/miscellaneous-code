85f20f85ccd85c4b7c8df4a7fcbbae36283b0383
gascsd <3311922913@qq.com>
2023-05-16 19:30:55 +0800

    RBTree红黑树插入的模拟实现与结构检测

--- RBTree/RBTree/RBTree.hpp
+++ RBTree/RBTree/RBTree.hpp
@@ -24,10 +24,11 @@ struct RBTreeNode
     RBTreeNode* _right;
     RBTreeNode* _parent;
     Color _col;
-    RBTreeNode(pair<K,V> kv)
-        :_kv(_kv)
+    RBTreeNode(const pair<K,V> kv)
+        :_kv(kv)
         ,_left(nullptr)
         ,_right(nullptr)
+        ,_parent(nullptr)
         ,_col(RED)
     {}
 };
@@ -37,7 +38,7 @@ class RBTree
 {
     typedef RBTreeNode<K, V> Node;
 public:
-    bool Insert(pair<K,V>& kv)
+    bool Insert(const pair<K,V>& kv)
     {
         if(_root == nullptr)
         {
@@ -66,7 +67,7 @@ public:
             }
         }
         cur = new Node(kv);
-        cur->col = RED;
+        cur->_col = RED;
         //连接上
         cur->_parent = parent;
         if(parent->_kv.first > cur->_kv.first)
@@ -78,7 +79,7 @@ public:
             parent->_right = cur;
         }
         //判断颜色是否合法
-        while(cur->_parent->col == RED)
+        while(parent && cur->_parent->_col == RED)
         {
             Node* grandfather = parent->_parent;
             if(parent == grandfather->_left)//当parent是grandfather左节点
@@ -88,7 +89,7 @@ public:
                 if(uncle && uncle->_col == RED)
                 {
                     grandfather->_col = RED;
-                    uncle->col = parent->_col = BLACK;
+                    uncle->_col = parent->_col = BLACK;
                     
                     cur = grandfather;
                     parent = cur->_parent;
@@ -120,7 +121,7 @@ public:
             {
                 Node* uncle = grandfather->_left;
                 //情况一：uncle存在且为红
-                if(uncle && uncle->col == RED)
+                if(uncle && uncle->_col == RED)
                 {
                     parent->_col = uncle->_col = BLACK;
                     grandfather->_col = RED;
@@ -151,24 +152,131 @@ public:
                 }
             }
         }
-        _root->col = BLACK;
+        _root->_col = BLACK;
         return true;
     }
-    void RotateL(Node* parent)
+    void RotateL(Node* parent)//左单旋
     {
-        Node* subL = parent->_left;
-        Node* subLR = subL->_right;
-        Node* ppNdoe = parent->_parent;
+        Node* subR = parent->_right;
+        Node* subRL = subR->_left;
+        Node* ppNode = parent->_parent;
         
-        //首先处理subLR
-        subLR->_parent = parent;
-        parent->_left = subLR;
-        
+        //处理subRL的部分
+        parent->_right = subRL;
+        if(subRL)
+            subRL->_parent = parent;
+        //处理parent和subR之间的关系
+        subR->_left = parent;
+        parent->_parent = subR;
+        //处理subR的parent
+        if(ppNode)//parent不是根节点的时候，需要处理parent的父亲节点
+        {
+            subR->_parent = ppNode;
+            if(ppNode->_left == parent)//parent是左节点
+            {
+                ppNode->_left = subR;
+            }
+            else//parent是右节点
+            {
+                ppNode->_right = subR;
+            }
+        }
+        else//parent是根节点的时候
+        {
+            _root = subR;
+            subR->_parent = nullptr;
+        }
     }
     void RotateR(Node* parent)
     {
+        Node* subL = parent->_left;
+        Node* subLR = subL->_right;
+        Node* ppNode = parent->_parent;
         
+        if(subLR)
+            subLR->_parent = parent;
+        parent->_left = subLR;
+        
+        subL->_right = parent;
+        parent->_parent = subL;
+        if(ppNode)
+        {
+            subL->_parent = ppNode;
+            if(ppNode->_left == parent)
+                ppNode->_left = subL;
+            else
+                ppNode->_right = subL;
+        }
+        else
+        {
+            _root = subL;
+            subL->_parent = nullptr;
+        }
+    }
+    void InOrder(vector<pair<K,V>>& v, Node* root)
+    {
+        if(root == nullptr)
+            return;
+        InOrder(v, root->_left);
+        v.push_back(root->_kv);
+        InOrder(v, root->_right);
+    }
+    bool isBSTree(Node* root)
+    {
+        vector<pair<K,V>> v;
+        InOrder(v,root);
+        for(size_t i = 0; i < v.size() - 1; ++i)
+        {
+            if(v[i].first > v[i+1].first)
+                return false;
+        }
+        return true;
+    }
+    bool Check(Node* root, int blackNum, int ref)
+    {
+        if(root == nullptr)
+        {
+            if(blackNum != ref)
+            {
+                cout << "count of black is error" << endl;
+                return false;
+            }
+            return true;
+        }
+        
+        if(root->_col == RED && root->_parent->_col == RED)
+        {
+            cout << "more red node" << endl;
+            return false;
+        }
+        if(root->_col == BLACK)
+            ++blackNum;
+        
+        return Check(root->_left, blackNum, ref)
+            && Check(root->_right, blackNum, ref);
+    }
+    bool isRBTree()
+    {
+        if(_root == nullptr)
+            return true;
+        if(isBSTree(_root))
+        {
+            if(_root->_col != BLACK)//判断性质2
+                return false;
+            int ref = 0;
+            Node* left = _root;
+            while(left)
+            {
+                if(left->_col == BLACK)
+                    ++ref;
+                left = left->_left;
+            }
+            
+            return Check(_root, 0, ref);
+        }
+        else
+            return false;
     }
 private:
-    Node* _root;
+    Node* _root = nullptr;
 };
--- RBTree/RBTree/main.cpp
+++ RBTree/RBTree/main.cpp
@@ -6,12 +6,43 @@
 //
 
 #include <iostream>
+#include <time.h>
+#include <vector>
 #include "RBTree.hpp"
+using namespace std;
 
+void TestRBTree1()
+{
+//    int a[] = { 8, 3, 1, 10, 6, 4, 7, 14, 13 };
+//    int a[] = { 16, 3, 7, 11, 9, 26, 18, 14, 15 };
+//    int a[] = { 4, 2, 6, 1, 3, 5, 15, 7, 16, 14 };
+    int a[] = { 57, 31, 71};
+    RBTree<int, int> t;
+    for (auto e : a)
+    {
+        t.Insert(make_pair(e, e));
+    }
+    cout << t.isRBTree() << endl;
+}
+void TestRBTree2()
+{
+    srand(time(0));
+    const size_t N = 10;
+    RBTree<int, int> t;
+    for (size_t i = 0; i < N; ++i)
+    {
+        size_t x = rand() % 100;
+        //cout << x << ":";
+        t.Insert(make_pair(x, x));
+        //cout << t.isRBTree() << endl;
+    }
+    cout << t.isRBTree() << endl;
+}
 
 
 int main()
 {
-    
+    TestRBTree1();
+    TestRBTree2();
     return 0;
 }
