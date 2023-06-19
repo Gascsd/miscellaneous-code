6e442e03ab7a1ada5de266293583a57fb4c70c2a
gascsd <3311922913@qq.com>
2023-06-20 02:05:45 +0800

    unordered_set的实现

--- unordered系列容器模拟实现/unordered系列容器模拟实现/unordered_set.hpp
+++ unordered系列容器模拟实现/unordered系列容器模拟实现/unordered_set.hpp
@@ -21,6 +21,20 @@ class unordered_set
         }
     };
 public:
+    typedef typename zht::BucketHash::HashTable<K, K, SetKeyOfT>::iteraotr iterator;
+    iterator begin()
+    {
+        return _ht.begin();
+    }
+    iterator end()
+    {
+        return _ht.end();
+    }
+    
+    std::pair<iterator,bool> insert(const K& key)
+    {
+        return _ht.Insert(key);
+    }
     
 private:
     BucketHash::HashTable<K, K, SetKeyOfT> _ht;
