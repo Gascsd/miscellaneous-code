ad9a001c25d482edc2d75662e15139171cead73b
gascsd <3311922913@qq.com>
2023-07-14 15:14:12 +0800

    同步测试文件——C++11

---
+++ Test_7_13/Test_7_13/main.cpp
@@ -0,0 +1,87 @@
+//
+//  main.cpp
+//  Test_7_13
+//
+//  Created by zht on 2023/7/13.
+//
+
+#include <iostream>
+#include <string>
+#include "string.hpp"
+using namespace std;
+
+//void Test1()
+//{
+//    //左值
+//    int a = 1;
+//    double x = 1.1, y = 2.2;
+//    int* pb = new int(10);
+//    const int c = 2;
+//    //左值引用
+//    int& ra = a;
+//    int*& rpb = pb;
+//    const int& rc = c;
+//    int& pvalue = *pb;
+//    //右值
+//    10;
+//    x + y;
+//    min(x, y);
+//    //右值引用
+//    int&& rr1 = 10;
+//    int&& rr2 = x + y;
+//    int&& rr3 = min(x, y);
+//}
+
+void Test2()
+{
+    int x = 1, y = 2;
+    int&& rr1 = x + y;
+    cout << "x + y:" << x + y << endl;
+    cout << "rr1:" << rr1 << endl;
+    rr1 = 10;
+    cout << "rr1" << rr1 << endl;
+}
+//void Test3()
+//{
+//    int a = 10;
+//    int& ra1 = a;
+//    int& ra2 = 10;//右值引用不能引用左值，因此这行代码报错
+//
+//    const int& ra3 = 10;
+//    const int& ra4 = a;
+//}
+//void Test4()
+//{
+//    //右值引用只能引用右值，不能引用左值
+//    int&& r1 = 10;
+//    int a = 10;
+//    //int&& r2 = a;//右值引用引用左值报错
+//    //Xcode报错内容：Rvalue reference to type 'int' cannot bind to lvalue of type 'int'
+//    //右值引用能引用move之后的左值
+//    int&& r3 = std::move(a);
+//}
+
+template<class T>
+T func(const T& val)
+{
+    T ret;
+    //...
+    return ret;
+}
+
+
+
+
+void Test5()
+{
+    zht::string str;
+    str = zht::to_string(-1234);
+}
+
+
+
+int main()
+{
+    Test5();
+    return 0;
+}
