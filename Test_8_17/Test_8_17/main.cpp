cd785ca7c23fb070f1503129293da1c099660ea5
gascsd <3311922913@qq.com>
2023-08-19 23:51:38 +0800

    测试文件

---
+++ Test_8_17/Test_8_17/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test_8_17
+//
+//  Created by zht on 2023/8/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
