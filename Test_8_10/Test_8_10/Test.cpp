038ae51ad974304484dbcd1ccc4b41d129b837bb
Gascsd <3311922913@qq.com>
2023-08-21 23:47:17 +0800

    同步测试文件

--- Test_8_10/Test_8_10/Test.cpp
+++ Test_8_10/Test_8_10/Test.cpp
@@ -1,8 +1,84 @@
 #include <iostream>
 using namespace std;
 
+
+//double Division(int a, int b)
+//{
+//	// 当b == 0时抛出异常
+//	if (b == 0)
+//		throw "Division by zero condition!";
+//	else
+//		return ((double)a / (double)b);
+//}
+//void Func()
+//{
+//	int len, time;
+//	cin >> len >> time;
+//	cout << Division(len, time) << endl;
+//}
+//void Test1()
+//{
+//	try 
+//	{
+//		Func();
+//	}
+//	catch (const char* errmsg)
+//	{
+//		cout << errmsg << endl;
+//	}
+//	catch (...) 
+//	{
+//		cout << "unkown exception" << endl;
+//	}
+//}
+
+double Division(int a, int b)
+{
+	// 当b == 0时抛出异常
+	if (b == 0)
+		throw "Division by zero condition!";
+	else
+		return ((double)a / (double)b);
+}
+void Func()
+{
+	// 这里可以看到如果发生除0错误抛出异常，另外下面的array没有得到释放。
+	// 所以这里捕获异常后并不处理异常，异常还是交给外面处理，这里捕获了再重新抛出去
+	int* arr = new int[10];
+	try
+	{
+		int len, time;
+		cin >> len >> time;
+		cout << Division(len, time) << endl;
+	}
+	catch (...)
+	{
+		cout << "delete []" << arr << endl;
+		delete[] arr;
+		throw;
+	}
+	//...
+	cout << "delete []" << arr << endl;
+	delete[] arr;
+}
+void Test2()
+{
+	try
+	{
+		Func();
+	}
+	catch (const char* errmsg)
+	{
+		cout << errmsg << endl;
+	}
+	catch (...)
+	{
+		cout << "unkown exception" << endl;
+	}
+}
+
 int main()
 {
-
+	Test2();
 	return 0;
 }
