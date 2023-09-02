807ef7c92d621ab4f2d06e81a63c69e56ad4265d
gascsd <3311922913@qq.com>
2023-09-02 10:44:13 +0800

    test file

---
+++ Test_9_2/Test_9_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test_9_2
+//
+//  Created by zht on 2023/9/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
